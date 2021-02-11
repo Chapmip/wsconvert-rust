dd1056fd9a19ed205e639ac9cb37adc578d57f10
Ian Chapman <ian@chapmip.co.uk>
2021-02-11 15:49:49 +0000

    Add main code to read from stdin and write to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
+use std::io::{self, Read, Write};
+
 mod asciify;
 
-fn main() {
-    let mut buf = [ 185; 32 ];
-    println!("before conversion = {:?}", buf);
-    println!("length = {}", buf.len());
+const CHUNK_SIZE: usize = 16 * 1024;
 
-    let res = asciify::convert(&mut buf);
-    println!("after conversion = {:?}", res);
-    println!("length = {}", res.len());
+fn main() {
+    let mut buffer = [0; CHUNK_SIZE];
+    let mut total_input = 0;
+    let mut total_output = 0;
+    
+    loop {
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,             // No further input
+            Ok(x) => x,
+            Err(_) => break,            // Some kind of error
+        };
+        total_input += num_read;
+        dbg!(num_read);
+        
+        let conv = asciify::convert(&mut buffer[..num_read]);
+        let num_conv = conv.len();
+        total_output += num_conv;
+        dbg!(num_conv);
+        
+        io::stdout().write_all(&buffer[..num_conv]).unwrap();
+        
+        if num_conv < num_read {
+            break;                      // EOF encountered
+        }
+    }
+    eprintln!("total input bytes: {}", total_input);
+    eprintln!("total output bytes: {}", total_output);
 }
