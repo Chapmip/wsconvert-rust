c1566acabfc6b49334cd106fd35285889768c1ca
Ian Chapman <ian@chapmip.co.uk>
2021-02-12 15:52:01 +0000

    Add 'convert_file()' in asciify module

--- src/asciify.rs
+++ src/asciify.rs
@@ -1,10 +1,65 @@
-//! Module to convert 8-bit input bytes into 7-bit ASCII characters
+//! Module to convert 8-bit input data into 7-bit ASCII characters
 
-const EOF_BYTE: u8 = 0x1A;          // End of File (EOF) marker
-const ASCII_MASK: u8 = 0x7F;        // ASCII is 7-bit code -> bit mask
+use std::io::{self, Read, Write};
+
+const EOF_BYTE: u8 = 0x1A;              // End of File (EOF) marker
+const ASCII_MASK: u8 = 0x7F;            // Bit mask for 7-bit ASCII
+
+const CHUNK_SIZE: usize = 16 * 1024;    // Moderate sized buffer
 
 
-/// Converts to 7-bit ASCII format a chunk of data in a byte (u8) slice,
+/// Converts a stream of 8-bit input bytes into a stream of output
+/// bytes in 7-bit ASCII format, using `convert_chunk()` to transform
+/// data in chunks and detect an End of File marker
+///
+/// Returns `()` on success or a `std::io::Error` type on failure
+///
+/// # Arguments
+///
+/// * `input` - Source of bytes that implements `Read` trait
+/// * `output` - Destination for bytes that implements `Write` trait
+///
+/// # Examples
+/// ```
+/// use std::io;
+/// use asciify::{convert_file, convert_chunk};
+///
+/// let mut input = io::stdin();
+/// let mut output = io::stdout();
+/// convert_file(&mut input, &mut output).unwrap();
+/// ```
+pub fn convert_file(input: &mut impl Read, output: &mut impl Write)
+    -> io::Result<()> {
+    let mut buffer = [0; CHUNK_SIZE];
+    let mut total_input = 0;
+    let mut total_output = 0;
+    
+    loop {
+        let num_read = input.read(&mut buffer)?;
+        if num_read == 0 {
+            break;                      // No further input
+        }
+        total_input += num_read;
+        dbg!(num_read);
+        
+        let conv = convert_chunk(&mut buffer[..num_read]);
+        let num_conv = conv.len();
+        total_output += num_conv;
+        dbg!(num_conv);
+        
+        output.write_all(&buffer[..num_conv])?;
+        
+        if num_conv < num_read {
+            break;                      // EOF encountered
+        }
+    }
+    eprintln!("total input bytes: {}", total_input);
+    eprintln!("total output bytes: {}", total_output);
+    Ok(())
+}
+
+
+/// Converts a chunk of data in a byte (u8) slice to 7-bit ASCII format,
 /// modifying it in place and returning a potentially smaller slice
 ///
 /// Returns a byte (u8) slice up to an End of File marker (if present)
@@ -21,7 +76,8 @@ const ASCII_MASK: u8 = 0x7F;        // ASCII is 7-bit code -> bit mask
 ///
 /// # Examples
 /// ```
-/// use asciify::convert;
+/// use asciify::convert_chunk;
+///
 /// let mut buf = [ 0x41, 0xC2, 0x43, 0x1A, 0x45, 0xC6 ];
 /// assert_eq!(convert_chunk(&mut buf[..5]), [ 0x41, 0x42, 0x43 ]);
 /// ```
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
-use std::io::{self, Read, Write};
+use std::io;
 
 mod asciify;
 
-const CHUNK_SIZE: usize = 16 * 1024;
-
 fn main() {
-    let mut buffer = [0; CHUNK_SIZE];
-    let mut total_input = 0;
-    let mut total_output = 0;
+    let mut input = io::stdin();
+    let mut output = io::stdout();
     
-    loop {
-        let num_read = match io::stdin().read(&mut buffer) {
-            Ok(0) => break,             // No further input
-            Ok(x) => x,
-            Err(_) => break,            // Some kind of error
-        };
-        total_input += num_read;
-        dbg!(num_read);
-        
-        let conv = asciify::convert_chunk(&mut buffer[..num_read]);
-        let num_conv = conv.len();
-        total_output += num_conv;
-        dbg!(num_conv);
-        
-        io::stdout().write_all(&buffer[..num_conv]).unwrap();
-        
-        if num_conv < num_read {
-            break;                      // EOF encountered
-        }
-    }
-    eprintln!("total input bytes: {}", total_input);
-    eprintln!("total output bytes: {}", total_output);
+    asciify::convert_file(&mut input, &mut output)
+        .unwrap();
 }
