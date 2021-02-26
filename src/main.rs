82cb94a8960e804a13f197e9c608b2f385321f92
Ian Chapman <ian@chapmip.co.uk>
2021-02-26 15:05:55 +0000

    Move transform_file() out of main module into new ws_filters module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//! Main module of WordStar conversion command line utility
+
 mod asciify;
 mod control_count;
 mod uni_chars;
@@ -5,83 +7,18 @@ mod ws_align;
 mod ws_chars;
 mod ws_control;
 mod ws_dot_cmd;
+mod ws_filters;
 mod ws_mappings;
 mod ws_overline;
 mod ws_special;
 mod ws_string;
 mod ws_wrappers;
 
-use control_count::ControlCount;
 use std::io::{self, Seek, SeekFrom};
 
-// TESTING CODE HERE FOR NOW...
-
-use std::io::{BufRead, BufReader, BufWriter, Read, Write}; // + self
-
-fn transform_file(input: &mut impl Read, output: &mut impl Write) -> io::Result<()> {
-    let mut original_counts = ControlCount::new("Original".to_string());
-    let mut post_dot_counts = ControlCount::new("Dot cmds".to_string());
-    let mut alignment_counts = ControlCount::new("Re-align".to_string());
-    let mut special_counts = ControlCount::new("Specials".to_string());
-    let mut overline_counts = ControlCount::new("Overline".to_string());
-    let mut wrappers_counts = ControlCount::new("Wrappers".to_string());
-    let mut de_ctrl_counts = ControlCount::new("Controls".to_string());
-
-    let reader = BufReader::new(input);
-    let mut writer = BufWriter::new(output);
-    let mut wrappers = ws_wrappers::Wrappers::new();
-
-    for line in reader.lines() {
-        let mut line = line?;
-        original_counts.scan(&line);
-
-        if let Some(replacement) = ws_dot_cmd::process(&line) {
-            match &replacement[..] {
-                "" => continue,
-                _ => line = replacement,
-            }
-        }
-        post_dot_counts.scan(&line);
-
-        if let Some(replacement) = ws_align::process(&line) {
-            line = replacement;
-        }
-        alignment_counts.scan(&line);
-
-        if let Some(replacement) = ws_special::process(&line) {
-            line = replacement;
-        }
-        special_counts.scan(&line);
-
-        if let Some(replacement) = ws_overline::process(&line) {
-            line = replacement;
-        }
-        overline_counts.scan(&line);
-
-        if let Some(replacement) = wrappers.process(&line) {
-            line = replacement;
-        }
-        wrappers_counts.scan(&line);
-
-        if let Some(replacement) = ws_control::process(&line, true) {
-            line = replacement;
-        }
-        de_ctrl_counts.scan(&line);
-
-        writeln!(writer, "{}", line)?;
-    }
-    writer.flush()?;
-
-    eprintln!("{}", original_counts);
-    eprintln!("{}", post_dot_counts);
-    eprintln!("{}", alignment_counts);
-    eprintln!("{}", special_counts);
-    eprintln!("{}", overline_counts);
-    eprintln!("{}", wrappers_counts);
-    eprintln!("{}", de_ctrl_counts);
-    Ok(())
-}
-
+/// Attempts to convert a WordStar file presented to `stdin` into a
+/// Unicode based text file on `stdout` via a temporary file
+///
 fn main() {
     let mut input = io::stdin();
     let mut output = tempfile::tempfile().expect("Cannot open temp file");
@@ -90,5 +27,5 @@ fn main() {
     let mut input = output;
     input.seek(SeekFrom::Start(0)).unwrap();
     let mut output = io::stdout();
-    transform_file(&mut input, &mut output).unwrap();
+    ws_filters::transform_file(&mut input, &mut output).unwrap();
 }
---
+++ src/ws_filters.rs
@@ -0,0 +1,95 @@
+//! Module to invoke WordStar format filters from input to output stream
+
+use crate::control_count::ControlCount;
+use crate::ws_align;
+use crate::ws_control;
+use crate::ws_dot_cmd;
+use crate::ws_overline;
+use crate::ws_special;
+use crate::ws_wrappers;
+use std::io::{self, BufRead, BufReader, BufWriter, Read, Write};
+
+/// Transforms a line-formatted stream of 7-bit ASCII input characters
+/// (e.g. from `asciify::convert_file`) into a line-formatted stream of
+/// Unicode output characters that implement conversions of WordStar dot
+/// commands, wrapper control characters and other special sequences
+///
+/// Returns `()` on success or a `std::io::Error` type on failure
+///
+/// # Arguments
+///
+/// * `input` - Source of bytes that implements `Read` trait
+/// * `output` - Destination for bytes that implements `Write` trait
+///
+/// # Examples
+/// ```
+/// use std::io;
+/// use ws_filters::transform_file;
+///
+/// let mut input = io::stdin();
+/// let mut output = io::stdout();
+/// transform_file(&mut input, &mut output).unwrap();
+/// ```
+pub fn transform_file(input: &mut impl Read, output: &mut impl Write) -> io::Result<()> {
+    let mut original_counts = ControlCount::new("Original".to_string());
+    let mut post_dot_counts = ControlCount::new("Dot cmds".to_string());
+    let mut alignment_counts = ControlCount::new("Re-align".to_string());
+    let mut special_counts = ControlCount::new("Specials".to_string());
+    let mut overline_counts = ControlCount::new("Overline".to_string());
+    let mut wrappers_counts = ControlCount::new("Wrappers".to_string());
+    let mut de_ctrl_counts = ControlCount::new("Controls".to_string());
+
+    let reader = BufReader::new(input);
+    let mut writer = BufWriter::new(output);
+    let mut wrappers = ws_wrappers::Wrappers::new();
+
+    for line in reader.lines() {
+        let mut line = line?;
+        original_counts.scan(&line);
+
+        if let Some(replacement) = ws_dot_cmd::process(&line) {
+            match &replacement[..] {
+                "" => continue,
+                _ => line = replacement,
+            }
+        }
+        post_dot_counts.scan(&line);
+
+        if let Some(replacement) = ws_align::process(&line) {
+            line = replacement;
+        }
+        alignment_counts.scan(&line);
+
+        if let Some(replacement) = ws_special::process(&line) {
+            line = replacement;
+        }
+        special_counts.scan(&line);
+
+        if let Some(replacement) = ws_overline::process(&line) {
+            line = replacement;
+        }
+        overline_counts.scan(&line);
+
+        if let Some(replacement) = wrappers.process(&line) {
+            line = replacement;
+        }
+        wrappers_counts.scan(&line);
+
+        if let Some(replacement) = ws_control::process(&line, true) {
+            line = replacement;
+        }
+        de_ctrl_counts.scan(&line);
+
+        writeln!(writer, "{}", line)?;
+    }
+    writer.flush()?;
+
+    eprintln!("{}", original_counts);
+    eprintln!("{}", post_dot_counts);
+    eprintln!("{}", alignment_counts);
+    eprintln!("{}", special_counts);
+    eprintln!("{}", overline_counts);
+    eprintln!("{}", wrappers_counts);
+    eprintln!("{}", de_ctrl_counts);
+    Ok(())
+}
